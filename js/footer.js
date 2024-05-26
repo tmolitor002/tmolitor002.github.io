54f79e59df4699b536b32d2bf5f06ed2e7aa08d4
tmolitor002 <molitor.tom@icloud.com>
2024-05-26 18:48:22 -0500

    Added header and footer js to use consistent header and footers instead of on each page

---
+++ js/footer.js
@@ -0,0 +1,27 @@
+document.write(`
+
+    <footer class="footer">
+    <div class="footer-wrapper">
+    <div class="footer-item">
+        <p>© 2024 by Tom Molitor |  <a href="mailto:tommymolitor@gmail.com">tommymolitor@gmail.com</a></p>
+    </div>
+    <div class="footer-item">
+        <ul class="socials">
+        <li>
+            <a href="https://www.linkedin.com/in/thomasmolitorjr" target="_blank"><img src="images/icon-logo-linkedin.png" alt="My LinkedIn profile"></a>
+        </li>
+        <li>
+            <a href="https://public.tableau.com/app/profile/tom.molitor/vizzes" target="_blank"><img src="images/icon-logo-tableau.png" alt="My Tableau Public profile"></a>
+        </li>
+        <li>
+            <a href="https://www.youtube.com/watch?v=VhunTRkuOH8" target="_blank"><img src="images/icon-logo-youtube.png" alt="YouTube Demo"></a>
+        </li>       
+        <li>
+            <a href="https://github.com/tmolitor002" target="_blank"><img src="images/github-mark-white.png" alt="My GitHub profile"></a>
+        </li>
+        </ul>
+    </div>
+    </div>
+    </footer>
+
+`);
