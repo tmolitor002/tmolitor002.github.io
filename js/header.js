1d58ab77152995b4c7919ea046c1e1814e95584b
Tom Molitor <47835001+tmolitor002@users.noreply.github.com>
2024-09-28 12:18:56 -0500

    removed www folder and updated navigation

--- js/header.js
+++ js/header.js
@@ -3,10 +3,10 @@ document.write(`
 <nav class="site-header">
 <h2>Tom Molitor</h2>
 <ul class="site-pages">
-    <li><a class="btn" href="index.html"><h4>Home</h4></a></li>
-    <li><a class="btn" href="resume.html"><h4>Resume</h4></a></li>
-    <li><a class="btn" href="food-deserts.html"><h4>Case Study</h4></a></li> <!-- eventually adjust to multiple case studies -->
-    <li><a class="btn" href="jack.html"><h4>Jack</h4></a></li>
+    <li><a class="btn" href="./index.html"><h4>Home</h4></a></li>
+    <li><a class="btn" href="./resume.html"><h4>Resume</h4></a></li>
+    <li><a class="btn" href="./food-deserts.html"><h4>Case Study</h4></a></li> <!-- eventually adjust to multiple case studies -->
+    <li><a class="btn" href="./jack.html"><h4>Jack</h4></a></li>
 </ul>
 </nav>
 
